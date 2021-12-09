92c6973a60131aa55f20fb5e0f682602f251bef8
vinhnado0231 <vinh.nd0231@gmail.com>
2021-12-09 21:38:51 +0700

    add file

--- module3/ss2_thiet_ke_va_tao_csdl/bai_tap/XayDungCSDLQuanLiBanHang.sql
+++ module3/ss2_thiet_ke_va_tao_csdl/bai_tap/XayDungCSDLQuanLiBanHang.sql
@@ -1,13 +1,10 @@
 create database QuanLiBanHang;
-
 use QuanLiBanHang;
-
 create table Customer(
 cID int primary key auto_increment,
 cName varchar(100),
 cAge int
 );
-
 create table `Order` (
 oID int primary key auto_increment,
 cID int,
@@ -15,18 +12,15 @@ foreign key(cID) references Customer(cID),
 oDate date,
 oTotalPrice int
 );
-
 create table Product (
 pID int primary key auto_increment,
 pName varchar(100),
 pPrice int
 );
-
 create table OrderDetail(
 oID int ,
-cID int ,
-primary key(oID,cID),
-foreign key(cID) references Customer(cID),
+pID int ,
+foreign key(pID) references Product(pID),
 foreign key(oID) references `Order`(oID),
 odQTY int
 );
--- module3/ss3_thao_tac_voi_csdl/bai_tap/ThaoTacVoiCSDLQuanLiBanHang.sql
+++ module3/ss3_thao_tac_voi_csdl/bai_tap/ThaoTacVoiCSDLQuanLiBanHang.sql
@@ -11,7 +11,6 @@ insert into Product(pName,pPrice) values
 ("May giat",3),
 ("Tu lanh",5),
 ("Dieu hoa",7),
-("May giat",3),
 ("Quat",1),
 ("Bep Dien",2);
 
@@ -26,12 +25,29 @@ insert into orderdetail values
 (1,4,2),
 (2,1,1),
 (3,1,8),
-(2,5,4);
+(2,5,4),
+(2,3,3);
 
 select oId,oDate,oTotalPrice from `order` ;
 
+select customer.* from customer
+inner join `order` on customer.cId=`order`.cID 
+join orderdetail on orderdetail.oId=`order`.oId
+join product on product.pID=orderdetail.pID
+where  customer.cId=`order`.oID and `order`.oId=orderdetail.oId and product.pID=orderdetail.pID
+group by cId,pId;
+
 select cName from Customer
 where cId not in (select cId from `order`);
+
+
+select `order`.oId,oDate,sum(odQTY * pPrice) as `Total Price` from `order`
+join Orderdetail on `order`.oId=Orderdetail.oId
+join Product on Orderdetail.pId=Product.pId
+group by `order`.oId;
+
+
+
 
 
 
