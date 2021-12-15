b6d7e89ead740f9fd1f17cb7f58bb50e9afa3271
vinhnado0231 <vinh.nd0231@gmail.com>
2021-12-15 18:12:54 +0700

    add file

--- module3/case_study1/DuLieu.sql
+++ module3/case_study1/DuLieu.sql
@@ -17,6 +17,7 @@ values
 (3,"Phuc vu"),
 (4,"Quan ly");
 
+
 insert into nhan_vien
 values
 (1,"Nguyen Van An", "1970-11-07",456231786,10000000,"0901234121","annguyen@gmail.com","295 Nguyễn Tất Thành,Đà Nẵng",1,3,1),
@@ -29,28 +30,6 @@ values
 (8,"Nguyễn Hà Đông","1989-09-03","234414123","9000000","0642123111","donghanguyen@gmail.com","111 Hùng Vương, Hà Nội",2,4,4),
 (9,"Tòng Hoang","1982-09-03","256781231","6000000","0245144444","hoangtong@gmail.com","	213 Hàm Nghi, Đà Nẵng",2,4,4),
 (10,"Nguyễn Công Đạo","	1994-01-08","755434343","8000000","	0988767111","nguyencongdao12@gmail.com","6 Hoà Khánh, Đồng Nai",2,3,2);
-
-
-insert into loai_khach
-values
-(1,"Diamond"),
-(2,"Platinium"),
-(3,"Gold"),
-(4,"Silver"),
-(5,"Member");
-
-insert into khach_hang
-values
-(1,	5,"Nguyễn Thị Hào","1970-11-07",0,	643431213,	945423362	,"thihao07@gmail.com","23 Nguyễn Hoàng, Đà Nẵng"),
-(2,	3,"Phạm Xuân Diệu","1992-08-08",1,865342123,0954333333,"xuandieu92@gmail.com","	K77/22 Thái Phiên, Quảng Trị"),
-(3,		1,"Trương Đình Nghệ","	1990-02-27",1,488645199,0373213122	,"nghenhan2702@gmail.com","	K323/12 Ông Ích Khiêm, Vinh"),
-(4,1	,"Dương Văn Quan	","1981-07-08",1,543432111,	0490039241	,"duongquan@gmail.com","K453/12 Lê Lợi, Đà Nẵng"),
-(5,4	,"Hoàng Trần Nhi Nhi","	1995-12-09",0,795453345,	0312345678	,"nhinhi123@gmail.com","224 Lý Thái Tổ, Gia Lai"),
-(6,4,"	Tôn Nữ Mộc Châu","	2005-12-06",0,732434215,	0988888844	,"tonnuchau@gmail.com","37 Yên Thế, Đà Nẵng	"),
-(7,1,"	Nguyễn Mỹ Kim	","1984-04-08",0,856453123,	0912345698,"	kimcuong84@gmail.com","	K123/45 Lê Lợi, Hồ Chí Minh"),
-(8,3,"	Nguyễn Thị Hào	","1999-04-08",	0,965656433,	0763212345	,"haohao99@gmail.com","	55 Nguyễn Văn Linh, Kon Tum"),
-(9,1,"	Trần Đại Danh	","1994-07-01",1,432341235,	0643343433	,"danhhai99@gmail.com","24 Lý Thường Kiệt, Quảng Ngãi"),
-(10,2,"	Nguyễn Tâm Đắc	","1989-07-01",1,344343432,	0987654321,"dactam@gmail.com","	22 Ngô Quyền, Đà Nẵng");
 
 insert into loai_khach
 values
@@ -85,7 +64,7 @@ insert into loai_dich_vu values
 (2	,"House"),
 (3	,"Room");
 
-insert into dich_vu (ma_dich_vu,ten_dich_vu,mo_ta_tien_nghi_khac,dien_tich_ho_boi,so_tang,ma_kieu_thue,ma_loai_dich_vu)
+insert into dich_vu (ma_dich_vu,ten_dich_vu,dien_tich,chi_phi_thue,so_nguoi_toi_da,tieu_chuan_phong,mo_ta_tien_nghi_khac,dien_tich_ho_boi,so_tang,ma_kieu_thue,ma_loai_dich_vu)
 values 
 (1,	"Villa Beach Front",25000,	10000000,	10,"vip","	Có hồ bơi",	500	,4	,3,	1),
 (2,	"House Princess 01",14000,	5000000	,7	,"vip","Có thêm bếp nướng",	null,	3,	2,	2),
@@ -96,7 +75,7 @@ values
 
 
 
-insert into dich_vu_di_kem(ma_dich_vu_di_kem,ten_dich_vu_di_kem,gia,don_vi,trang_tinh)
+insert into dich_vu_di_kem(ma_dich_vu_di_kem,ten_dich_vu_di_kem,gia,don_vi,trang_thai)
 values
 (1,"Karaoke",10000,"giờ","tiện nghi hiện tại"),
 (2,"Thuê xe máy",10000,"chiếc"," hỏng 1 xe"),
@@ -130,3 +109,4 @@ values
 (6	,1,	1,	3),
 (7,	2	,1	,2),
 (8,	2,	12,	2);
+
---
+++ module3/case_study1/Task.sql
@@ -0,0 +1,57 @@
+-- Yeu cau 2:
+select nhan_vien.* from nhan_vien
+where (ho_ten like 'H%' or ho_ten like 'K%' or ho_ten like 'T%')and length(ho_ten)<=15;
+
+-- Yeu cau 3:
+select khach_hang.* from khach_hang
+where (year(curdate())-year(ngay_sinh) between 18 and 50) 
+and (dia_chi like "%Quang Tri" or dia_chi like "%Da Nang"); 
+
+-- Yeu cau 4:
+select khach_hang.ma_khach_hang,khach_hang.ho_ten, count(hop_dong.ma_khach_hang) as so_lan_dat_phong from khach_hang
+inner join hop_dong on khach_hang.ma_khach_hang=hop_dong.ma_khach_hang
+where ma_loai_khach in (select ma_loai_khach from loai_khach where ten_loai_khach="Diamond")
+group by hop_dong.ma_khach_hang
+order by so_lan_dat_phong asc;
+    
+-- Yeu cau 5:
+select khach_hang.ma_khach_hang, ho_ten, ten_loai_khach,hop_dong.ma_hop_dong, ten_dich_vu, ngay_lam_hop_dong, ngay_ket_thuc,
+sum(dich_vu.chi_phi_thue+hop_dong_chi_tiet.so_luong*dich_vu_di_kem.gia) as tong_tien
+from khach_hang left join hop_dong on khach_hang.ma_khach_hang=hop_dong.ma_khach_hang
+left join loai_khach on loai_khach.ma_loai_khach=khach_hang.ma_loai_khach
+left join dich_vu on hop_dong.ma_dich_vu=dich_vu.ma_dich_vu
+left join hop_dong_chi_tiet on hop_dong.ma_hop_dong=hop_dong_chi_tiet.ma_hop_dong
+left join dich_vu_di_kem on dich_vu_di_kem.ma_dich_vu_di_kem=hop_dong_chi_tiet.ma_dich_vu_di_kem
+group by hop_dong.ma_hop_dong;
+
+    
+-- Yeu cau 6:
+select dich_vu.ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu from dich_vu 
+inner join loai_dich_vu on dich_vu.ma_loai_dich_vu=loai_dich_vu.ma_loai_dich_vu
+where dich_vu.ma_dich_vu not in (select ma_dich_vu from hop_dong where (month(ngay_lam_hop_dong)between 1 and 3)
+and year(ngay_lam_hop_dong));
+  
+  -- Yeu cau 7:
+  select ma_dich_vu, ten_dich_vu, dien_tich, so_nguoi_toi_da, chi_phi_thue, ten_loai_dich_vu
+  from dich_vu 
+  inner join loai_dich_vu on dich_vu.ma_loai_dich_vu=loai_dich_vu.ma_loai_dich_vu
+  where dich_vu.ma_dich_vu in (select ma_dich_vu from hop_dong where year(ngay_lam_hop_dong)=2020) and
+  dich_vu.ma_dich_vu not in (select ma_dich_vu from hop_dong where year(ngay_lam_hop_dong)=2021);
+  
+  -- Yeu cau 8:
+  -- c1
+  select distinct (ho_ten) as ten_khach_hang from khach_hang
+  order by ho_ten;
+  -- c2:
+  select ho_ten from khach_hang
+ group by ho_ten;
+  -- c3:
+  select ho_ten from khach_hang
+  union
+  select ho_ten from khach_hang;
+-- yeu cau 9:
+select month(ngay_lam_hop_dong) as thang,count(hop_dong.ma_dich_vu) as so_luong_khach_hang from dich_vu
+inner join hop_dong on hop_dong.ma_dich_vu=dich_vu.ma_dich_vu
+where year(ngay_lam_hop_dong)=2021
+group by month(ngay_lam_hop_dong)
+order by month(ngay_lam_hop_dong);
