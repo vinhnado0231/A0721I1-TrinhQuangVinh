0e4e39a7ba6d4c18e761463632a42446d662efca
vinhnado0231 <vinh.nd0231@gmail.com>
2021-12-20 18:54:06 +0700

    add file

--- module3/case_study1/Task.sql
+++ module3/case_study1/Task.sql
@@ -31,27 +31,42 @@ inner join loai_dich_vu on dich_vu.ma_loai_dich_vu=loai_dich_vu.ma_loai_dich_vu
 where dich_vu.ma_dich_vu not in (select ma_dich_vu from hop_dong where (month(ngay_lam_hop_dong)between 1 and 3)
 and year(ngay_lam_hop_dong));
   
-  -- Yeu cau 7:
-  select ma_dich_vu, ten_dich_vu, dien_tich, so_nguoi_toi_da, chi_phi_thue, ten_loai_dich_vu
-  from dich_vu 
-  inner join loai_dich_vu on dich_vu.ma_loai_dich_vu=loai_dich_vu.ma_loai_dich_vu
-  where dich_vu.ma_dich_vu in (select ma_dich_vu from hop_dong where year(ngay_lam_hop_dong)=2020) and
-  dich_vu.ma_dich_vu not in (select ma_dich_vu from hop_dong where year(ngay_lam_hop_dong)=2021);
+-- Yeu cau 7:
+select ma_dich_vu, ten_dich_vu, dien_tich, so_nguoi_toi_da, chi_phi_thue, ten_loai_dich_vu
+from dich_vu 
+inner join loai_dich_vu on dich_vu.ma_loai_dich_vu=loai_dich_vu.ma_loai_dich_vu
+where dich_vu.ma_dich_vu in (select ma_dich_vu from hop_dong where year(ngay_lam_hop_dong)=2020) and
+dich_vu.ma_dich_vu not in (select ma_dich_vu from hop_dong where year(ngay_lam_hop_dong)=2021);
   
-  -- Yeu cau 8:
-  -- c1
-  select distinct (ho_ten) as ten_khach_hang from khach_hang
-  order by ho_ten;
-  -- c2:
-  select ho_ten from khach_hang
- group by ho_ten;
-  -- c3:
-  select ho_ten from khach_hang
-  union
-  select ho_ten from khach_hang;
+-- Yeu cau 8:
+-- c1:
+select ho_ten from khach_hang
+union
+select ho_ten from khach_hang;
+  
+-- c2:
+select ho_ten from khach_hang
+group by ho_ten;
+
 -- yeu cau 9:
 select month(ngay_lam_hop_dong) as thang,count(hop_dong.ma_dich_vu) as so_luong_khach_hang from dich_vu
 inner join hop_dong on hop_dong.ma_dich_vu=dich_vu.ma_dich_vu
 where year(ngay_lam_hop_dong)=2021
 group by month(ngay_lam_hop_dong)
 order by month(ngay_lam_hop_dong);
+
+-- yeu cau 10:
+select hop_dong.ma_hop_dong, ngay_lam_hop_dong, ngay_ket_thuc, tien_dat_coc, sum(so_luong) as so_luong_dich_vu_di_kem 
+from hop_dong 
+left join hop_dong_chi_tiet on hop_dong.ma_hop_dong=hop_dong_chi_tiet.ma_hop_dong
+left join dich_vu_di_kem on hop_dong_chi_tiet.ma_dich_vu_di_kem=dich_vu_di_kem.ma_dich_vu_di_kem
+group by hop_dong.ma_hop_dong; 
+ 
+-- Yeu cau 11:
+select dich_vu_di_kem.* from dich_vu_di_kem
+inner join hop_dong_chi_tiet on dich_vu_di_kem.ma_dich_vu_di_kem=hop_dong_chi_tiet.ma_dich_vu_di_kem
+inner join hop_dong on hop_dong.ma_hop_dong=hop_dong_chi_tiet.ma_hop_dong
+inner join khach_hang on khach_hang.ma_khach_hang=hop_dong.ma_khach_hang
+inner join loai_khach on loai_khach.ma_loai_khach=khach_hang.ma_loai_khach
+where ten_loai_khach="Diamond" and dia_chi like "%Vinh" or  dia_chi like "%Quang Ngai";
+ 
