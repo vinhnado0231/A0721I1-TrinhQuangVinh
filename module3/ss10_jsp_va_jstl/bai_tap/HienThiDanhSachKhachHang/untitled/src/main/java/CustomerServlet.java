75c1fb9c08f6ebda827671c1e2f753ba028f3c24
vinhnado0231 <vinh.nd0231@gmail.com>
2022-01-05 18:23:05 +0700

    add file

---
+++ module3/ss10_jsp_va_jstl/bai_tap/HienThiDanhSachKhachHang/untitled/src/main/java/CustomerServlet.java
@@ -0,0 +1,52 @@
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "CustomerServlet", value = "/CustomerServlet")
+public class CustomerServlet extends HttpServlet {
+    CustomerList list = new CustomerList();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        switch (action) {
+            case "create":
+                break;
+            case "edit":
+                break;
+            case "delete":
+                break;
+            case "view":
+                break;
+            default:
+                listCustomers(request, response);
+                break;
+        }
+    }
+
+    private void listCustomers(HttpServletRequest request, HttpServletResponse response) {
+        List<Customer> customers = list.getList();
+        request.setAttribute("customers", customers);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/index.jsp");
+        try {
+            dispatcher.forward(request, response);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
