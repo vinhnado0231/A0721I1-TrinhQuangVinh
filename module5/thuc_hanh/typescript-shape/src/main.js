341a099147d68dd8f3164886b9fc3969683f53e4
vinhnado0231 <vinh.nd0231@gmail.com>
2022-04-22 22:41:00 +0700

    add file

---
+++ module5/thuc_hanh/typescript-shape/src/main.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var rectangle_1 = require("./rectangle");
+var rectangle = new rectangle_1.Rectangle(5, 5, 10, 20);
+console.log(rectangle.toString());
+console.log(rectangle.area());
